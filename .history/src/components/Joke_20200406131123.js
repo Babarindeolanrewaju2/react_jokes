dc288532e36032ca0c019093f7b11453a7f763b3
Babarindeolanrewaju2 <Babarindeolanrewaju@gmail.com>
2019-07-06 21:34:05 -0600

    first commit

---
+++ .history/src/components/Joke_20200406131123.js
@@ -0,0 +1,3 @@
+const Joke = ({ joke: { setup, punchline } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
---
+++ .history/src/components/Joke_20200406131152.js
@@ -0,0 +1,3 @@
+export const Joke = ({ joke: { setup, punchline } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
